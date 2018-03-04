4f22e720604a177120cdade38e22b024aedf74ca
NATARAP <prabhakar.natarajan@elsevier.com>
2018-03-04 16:07:57 -0500

    initial commit

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name angularJsImageViewerApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the angularJsImageViewerApp
+ */
+angular.module('angularJsImageViewerApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
